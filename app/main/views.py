bf75b84202b6ef1353e0d4827bb2dab44b718a1f
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-29 14:28:17 +0300

    pushing autendication

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,10 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, request, redirect, url_for
 from . import main
+from flask_login import login_required
 
-# Views
 @main.route('/')
 def index():
-
     '''
     View root page function that returns the index page and its data
     '''
-    
     return render_template('index.html')
