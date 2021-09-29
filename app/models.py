3bd031436bf003ddf87b78a94b043d0cc278c830
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-09-29 14:10:46 +0300

    pushing migration

--- app/models.py
+++ app/models.py
@@ -1,4 +1,9 @@
-class User():
-    """
-    Class model for creating user instances
-    """
+from . import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
